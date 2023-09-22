3c2e42d99a7bc6e71ffe22dbc9fc982300ab31cc
danifgx <danifgx@gmail.com>
2023-09-22 19:33:14 +0200

    Testing initial and final states when clicking

--- memory-game/src/components/MemoryCard.test.jsx
+++ memory-game/src/components/MemoryCard.test.jsx
@@ -10,18 +10,23 @@ it("MemoryCard displays faces down", async () => {
   expect(screen.queryByText("2, 1")).not.toBeInTheDocument();
 });
 
-test("Button makes the card to face down - hide value", () => {
+test("Button makes the card to face up - show value, and then face down - hide value", () => {
   const position = [2, 1];
   render(<MemoryCard position={position} />);
-
   const notVisiblePositionBefore = screen.queryByText("2, 1");
+
   expect(notVisiblePositionBefore).toBeNull();
 
   const button = screen.getByRole("button", { name: "Click here" });
   fireEvent.click(button);
-
   const visiblePositionAfter = screen.getByText("2, 1");
+
   expect(visiblePositionAfter).toBeInTheDocument();
+
+  fireEvent.click(button);
+
+  const notVisiblePositionAfter = screen.queryByText("2, 1");
+  expect(notVisiblePositionAfter).toBeNull();
 });
 
 
